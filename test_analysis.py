646085a258b893bf0804b7ecb38b060265a27c56
mydrayaolga77-art <mydrayaolga77@gmail.com>
2026-01-03 22:29:24 +0500

    Update test_analysis.py

--- test_analysis.py
+++ test_analysis.py
@@ -5,10 +5,10 @@ def test_data_loading():
     """Проверяет, что файл загружается и содержит данные"""
     df = pd.read_csv('AI_Market_Report_2025_cleaned.csv')
     assert not df.empty, "Файл пуст!"
-    assert 'Salary' in df.columns, "В таблице нет колонки Salary"
+    assert 'salary_usd' in df.columns, "В таблице нет колонки salary_usd"
 
 def test_salary_type():
     """Проверяет, что зарплата — это число"""
     df = pd.read_csv('AI_Market_Report_2025_cleaned.csv')
     # Добавьте здесь логику проверки типов данных
-    assert df['Salary'].dtype in ['int64', 'float64']
+    assert df['salary_usd'].dtype in ['int64', 'float64']
